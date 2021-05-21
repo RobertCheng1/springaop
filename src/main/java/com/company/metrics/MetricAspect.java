81d65504abc9a5916e2edc26597dded120627bf7
unknown <chengpengxing@360buyAD.local>
2021-05-21 10:43:17 +0800

    精确理解了使用注解装配 AOP 的内容

--- src/main/java/com/company/metrics/MetricAspect.java
+++ src/main/java/com/company/metrics/MetricAspect.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MetricAspect {
-
-	// 注意 metric()方法标注了 @ Around("@annotation(metricTime)")，
+	// 注意 metric()方法标注了 @Around("@annotation(metricTime)")，
+	// ===注意不是 @Around("@annotation(MetricTime)")，准确的说这里应该是 metric 方法的第二个参数的参数名而不是参数类型===
 	// 它的意思是，符合条件的目标方法是带有 @MetricTime 注解的方法，
 	// 因为metric()方法参数类型是MetricTime（注意参数名是metricTime不是MetricTime），我们通过它获取性能监控的名称。
 	// 有了@MetricTime注解，再配合MetricAspect，任何Bean，只要方法标注了@MetricTime注解，就可以自动实现性能监控
